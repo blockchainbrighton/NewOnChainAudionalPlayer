41ee432c61e2ea233e0854f880c218af91723a84
blockchainbrighton <jim@blockchainbrighton.com>
2023-12-23 16:28:57 +0000

    updated logging to check function calls

--- index_combined.js
+++ index_combined.js
@@ -11,6 +11,20 @@ let isLooping = true;
 let isStoppedManually = false;
 let cumulativeOffset = 0;  // Added cumulative offset for seamless playback
 
+const functionCallTracker = {};
+
+const trackFunctionCall = (functionName) => {
+    if (functionCallTracker[functionName]) {
+        functionCallTracker[functionName]++;
+    } else {
+        functionCallTracker[functionName] = 1;
+    }
+};
+
+const reviewFunctionCalls = () => {
+    console.log('Function Call Tracker:', functionCallTracker);
+};
+
 const customLog = (message, isError = false) => {
     const logFunction = isError ? console.error : console.log;
     logFunction(message);
@@ -18,12 +32,15 @@ const customLog = (message, isError = false) => {
 
 // Function to convert base64 to an array buffer
 function base64ToArrayBuffer(base64) {
+    trackFunctionCall('base64ToArrayBuffer');
+    customLog('base64ToArrayBuffer entered');
     return Uint8Array.from(atob(base64), c => c.charCodeAt(0)).buffer;
 }
 
 
 // Function to decode audio data
 const decodeAudioData = (audioData) => {
+    trackFunctionCall('decodeAudioData');
     console.log('decodeAudioData entered');
     return new Promise((resolve, reject) => {
         audioContext.decodeAudioData(audioData, resolve, reject);
@@ -32,6 +49,7 @@ const decodeAudioData = (audioData) => {
 
 // Improved and optimized loadAudioFile function
 const loadAudioFile = async (url) => {
+    trackFunctionCall('loadAudioFile');
     console.log('loadAudioFile entered...');
     if (!url) {
         customLog('Encountered invalid or missing URL', true);
@@ -62,31 +80,25 @@ const loadAudioFile = async (url) => {
 
 // Handles direct audio files
 const processFile = async (response, type) => {
+    trackFunctionCall('processFile');
     try {
-        if (type === 'audio') {
-            const audioData = await response.arrayBuffer();
-            return decodeAudioData(audioData);
-        } else if (type === 'json') {
-            const jsonData = await response.json();
-            if (jsonData.audioData && typeof jsonData.audioData === 'string') {
-                console.log('Found base64 audio data:', jsonData.audioData);
-                const audioData = base64ToArrayBuffer(jsonData.audioData.split(',')[1]);
-                return decodeAudioData(audioData);
-            } else {
-                customLog('JSON does not contain base64 encoded audio data', true);
-                return null;
-            }
-        } else {
-            customLog('Unsupported file type for processing', true);
-            return null;
+        const data = type === 'audio' ? await response.arrayBuffer() : await response.json();
+        if (type === 'json' && data.audioData && typeof data.audioData === 'string') {
+            return decodeAudioData(base64ToArrayBuffer(data.audioData.split(',')[1]));
+        } else if (type === 'audio') {
+            return decodeAudioData(data);
         }
+        log('Invalid or unsupported data type', true);
+        return null;
     } catch (error) {
-        customLog(`Error processing file: ${error}`, true);
+        log(`Error processing file: ${error}`, true);
         return null;
     }
 };
 
 const calculateTrimTimes = (trimSetting, totalDuration) => {
+    trackFunctionCall('calculateTrimTimes');
+    customLog(`calculateTrimTimes entered for trim setting: ${JSON.stringify(trimSetting)}`);
     const startTime = Math.max(0, Math.min((trimSetting.startSliderValue / 100) * totalDuration, totalDuration));
     const endTime = (trimSetting.endSliderValue / 100) * totalDuration;
     return { startTime, duration: Math.max(0, endTime - startTime) };
@@ -96,6 +108,8 @@ const calculateStepTime = () => 60 / BPM / 4;
 
 // Modified to include cumulativeOffset in playbackTime
 const createAndStartAudioSource = (audioBuffer, trimSetting, playbackTime) => {
+    trackFunctionCall('createAndStartAudioSource');
+    customLog(`Creating and starting audio source. Active sources: ${activeSources.size}`);
     if (!audioBuffer) return;
 
     const source = audioContext.createBufferSource();
@@ -109,6 +123,8 @@ const createAndStartAudioSource = (audioBuffer, trimSetting, playbackTime) => {
 };
 
 const handleSourceEnd = (source) => {
+    trackFunctionCall('handleSourceEnd');
+    customLog(`Handling source end. Active sources remaining: ${activeSources.size}`);
     activeSources.delete(source);
     customLog(`Handling source end. Active sources remaining: ${activeSources.size}`);
     if (activeSources.size === 0 && isLooping && !isStoppedManually) {
@@ -120,12 +136,16 @@ const handleSourceEnd = (source) => {
 };
 
 const schedulePlaybackForStep = (audioBuffer, trimSetting, stepIndex) => {
+    trackFunctionCall('schedulePlaybackForStep');
+    customLog(`schedulePlaybackForStep entered with stepIndex: ${stepIndex}`);
     const playbackTime = stepIndex * calculateStepTime();
     createAndStartAudioSource(audioBuffer, trimSetting, playbackTime);
 };
 
 // Optimized playAudio function
 const playAudio = async () => {
+    trackFunctionCall('playAudio');
+    customLog('playAudio entered...');
     if (!validateSequenceData(sequenceData)) {
         return customLog("No valid sequence data available. Cannot play audio.", true);
     }
@@ -147,18 +167,26 @@ const playAudio = async () => {
 };
 
 const validateSequenceData = (data) => {
+    trackFunctionCall('validateSequenceData');
+    customLog(`validateSequenceData entered with data: ${JSON.stringify(data)}`);
     return data && data.projectURLs && data.projectSequences;
 };
 
 const loadAudioBuffers = async (urls) => {
+    trackFunctionCall('loadAudioBuffers');
+    customLog(`loadAudioBuffers entered with urls: ${JSON.stringify(urls)}`);
     return Promise.all(urls.map(loadAudioFile));
 };
 
 const isValidAudioBuffers = (buffers) => {
+    trackFunctionCall('isValidAudioBuffers');
+    customLog(`isValidAudioBuffers entered with buffers: ${JSON.stringify(buffers)}`);
     return buffers.some(buffer => buffer);
 };
 
 const scheduleSequences = (sequences, audioBuffers, trimSettings) => {
+    trackFunctionCall('scheduleSequences');
+    customLog(`ScheduleSequences entered with sequences: ${JSON.stringify(sequences)}`);
     Object.entries(sequences).forEach(([sequenceName, channels]) => {
         const sequenceDuration = 64 * calculateStepTime();
         scheduleChannels(channels, audioBuffers, trimSettings);
@@ -167,6 +195,8 @@ const scheduleSequences = (sequences, audioBuffers, trimSettings) => {
 };
 
 const scheduleChannels = (channels, audioBuffers, trimSettings) => {
+    trackFunctionCall('scheduleChannels');
+    customLog(`ScheduleChannels entered with channels: ${JSON.stringify(channels)}`);
     Object.entries(channels).forEach(([channelName, channelData], channelIndex) => {
         const steps = channelData.steps;
         const audioBuffer = audioBuffers[channelIndex];
@@ -182,6 +212,8 @@ const scheduleChannels = (channels, audioBuffers, trimSettings) => {
 };
 
 const handlePlaybackCompletion = () => {
+    trackFunctionCall('handlePlaybackCompletion');
+    customLog("HandlePlaybackCompletion entered");
     isStoppedManually = false;
     customLog("Scheduled playback for active steps in available sequences and channels");
     if (activeSources.size === 0 && isLooping) {
@@ -193,6 +225,8 @@ const handlePlaybackCompletion = () => {
 };
 
 const stopAudio = () => {
+    trackFunctionCall('stopAudio');
+    customLog("Stopping audio playback");
     activeSources.forEach(source => {
         source.stop();
         source.disconnect();
@@ -202,96 +236,79 @@ const stopAudio = () => {
 };
 
 const setupUIHandlers = () => {
-    // Ensure elements exist before adding event listeners
-    const playButton = document.getElementById('playButton');
-    const stopButton = document.getElementById('stopButton');
-    const fileInput = document.getElementById('fileInput');
+    trackFunctionCall('setupUIHandlers');
+    log('Setting up UI handlers');
 
-    if (playButton) {
-        playButton.addEventListener('click', () => {
-            
-            isLooping = true;
-            customLog('Play button pressed, attempting to start playback.');
-            playAudio();
-        });
-    }
+    const setupButtonHandler = (buttonId, handler) => {
+        const button = document.getElementById(buttonId);
+        if (button) {
+            button.addEventListener('click', handler);
+        }
+    };
 
-    if (stopButton) {
-        stopButton.addEventListener('click', () => {
-            isStoppedManually = true;
-            customLog('Stop button pressed, calling stopAudio.');
-            stopAudio();
-        });
-    }
+    const setupFileInputHandler = (inputId, handler) => {
+        trackFunctionCall('setupFileInputHandler');
+        const input = document.getElementById(inputId);
+        if (input) {
+            input.addEventListener('change', handler);
+        }
+    };
 
-    if (fileInput) {
-        fileInput.addEventListener('change', async (event) => {
-            try {
-                // Assuming processAndLoadAudio is defined elsewhere
-                sequenceData = await processAndLoadAudio(event.target.files[0], loadAudioFile);
-                if (sequenceData && sequenceData.projectURLs.some(url => url)) {
-                    playButton.disabled = false;
-                    customLog("File loaded successfully. Ready to play. Click the play button!");
-                } else {
-                    customLog("No valid audio URLs found in the sequence data.", true);
-                    playButton.disabled = true;
-                }
-            } catch (err) {
-                playButton.disabled = true;
-                customLog(`Error processing sequence data: ${err}`, true);
+    setupButtonHandler('playButton', () => {
+        isLooping = true;
+        log('Play button pressed, attempting to start playback.');
+        playAudio();
+    });
+
+    setupButtonHandler('stopButton', () => {
+        isStoppedManually = true;
+        log('Stop button pressed, calling stopAudio.');
+        stopAudio();
+    });
+
+    setupFileInputHandler('fileInput', async (event) => {
+        try {
+            sequenceData = await processAndLoadAudio(event.target.files[0], loadAudioFile);
+            const playButton = document.getElementById('playButton');
+            if (sequenceData && sequenceData.projectURLs.some(url => url)) {
+                if (playButton) playButton.disabled = false;
+                log("File loaded successfully. Ready to play. Click the play button!");
+            } else {
+                if (playButton) playButton.disabled = true;
+                log("No valid audio URLs found in the sequence data.", true);
             }
-        });
-    }
+        } catch (err) {
+            const playButton = document.getElementById('playButton');
+            if (playButton) playButton.disabled = true;
+            log(`Error processing sequence data: ${err}`, true);
+        }
+    });
 };
+
 
 // loadJsonFromLocal.js
 
 const log = (message, isError = false) => console[isError ? 'error' : 'log'](message);
 
 const validateAudioData = (data) => {
+    trackFunctionCall('validateAudioData');
+    customLog(`Validating JSON file entered:`);
     if (!data.trimSettings || !data.projectSequences?.Sequence0?.ch0?.steps || data.projectSequences.Sequence0.ch0.steps.length !== 64 || !data.projectBPM) {
         throw new Error('Invalid or missing data in JSON');
     }
 };
 
 const readFileAsJSON = (file) => new Promise((resolve, reject) => {
+    trackFunctionCall('readFileAsJSON');
+    customLog(`Reading JSON file entered:`);
     const reader = new FileReader();
     reader.onload = e => resolve(JSON.parse(e.target.result));
     reader.onerror = err => reject(err);
     reader.readAsText(file);
 });
 
-// const analyzeJSONFormat = (data) => {
-//     log('Analyzing JSON format and content:', false);
-// 
-//     // Loop through each URL in the data and analyze its content
-//     if (data.projectURLs) {
-//         data.projectURLs.forEach(async (url, index) => {
-//             if (typeof url === 'string' && url.trim() !== '') {
-//                 try {
-//                     const response = await fetch(url, { method: 'HEAD' }); // Using HEAD to get headers without downloading the whole file
-//                     const contentType = response.headers.get('content-type');
-// 
-//                     if (contentType.includes('audio/')) {
-//                         log(`URL ${index} is direct audio: ${url} with type ${contentType}`);
-//                     } else if (contentType.includes('application/json')) {
-//                         log(`URL ${index} is a JSON file that might contain audio data: ${url}`);
-//                     } else {
-//                         log(`URL ${index} is of unknown type: ${url}`);
-//                     }
-//                 } catch (error) {
-//                     log(`Error analyzing URL ${index}: ${url} with error: ${error}`, true);
-//                 }
-//             } else {
-//                 log(`URL ${index} is invalid or empty`, true);
-//             }
-//         });
-//     } else {
-//         log('No projectURLs found in the data to analyze.', true);
-//     }
-// };
-
 const processAndLoadAudio = async (file, loadAudioFile) => {
+    trackFunctionCall('processAndLoadAudio');
     log(`Processing JSON file: ${file.name}`);
     try {
         const sequenceData = await readFileAsJSON(file);
